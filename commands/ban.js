c13cb6a5313ddefe642113f48b0eb1d27168aa2f
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2020-07-16 22:44:29 -0700

    added ban command, recreated gitignore, added package-lock.json, and formatted package.json.

---
+++ commands/ban.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const fs = require("graceful-fs");
+
+
+module.exports.run = async (client, message, args) => {
+    if (!args || !args[0]) return;
+    const user = args[0];
+    if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+            member
+                .ban({
+                    reason: args[1],
+                })
+                .then(() => {
+                    message.channel.send(`Successfully banned ${user.tag}`)
+                })
+                .catch(err => {
+                    message.reply("I was unable to ban the member");
+                    message.channel.send(err);
+                    console.error(err);
+                });
+        } else {
+            message.channel.send("That user isn't in this guild!");
+        }
+    } else {
+        message.channel.send("You didn't mention the user to ban!")
+    }
+}
+
+module.exports.config = {
+    name: "ban",
+    aliases: [],
+    use: "ban [username] [reason]",
+    description: "Bans member",
+    state: "gamma",
+    page: 2
+};
