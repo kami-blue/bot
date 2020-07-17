49cfbac8e846b0e469103d4492bb227f419337d7
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2020-07-16 23:14:24 -0700

    im big dum dum

--- commands/ban.js
+++ commands/ban.js
@@ -3,28 +3,31 @@ const fs = require("graceful-fs");
 
 
 module.exports.run = async (client, message, args) => {
-    if (!args || !args[0]) return;
-    const user = args[0];
-    if (user) {
-        const member = message.guild.member(user);
-        if (member) {
-            member
-                .ban({
-                    reason: args[1],
-                })
-                .then(() => {
-                    message.channel.send(`Successfully banned ${user.tag}`)
-                })
-                .catch(err => {
-                    message.reply("I was unable to ban the member");
-                    message.channel.send(err);
-                    console.error(err);
-                });
+    const sender = message.sender;
+    if(sender.hasPermission('BAN_MEMBERS')) {
+        if (!args || !args[0]) return;
+        const user = args[0];
+        if (user) {
+            const member = message.guild.member(user);
+            if (member) {
+                member
+                    .ban({
+                        reason: args[1].toString(),
+                    })
+                    .then(() => {
+                        message.channel.send(`Successfully banned ${user.tag}`)
+                    })
+                    .catch(err => {
+                        message.channel.send("I was unable to ban the member:");
+                        message.channel.send(err);
+                        console.error(err);
+                    });
+            } else {
+                message.channel.send("That user isn't in this guild!");
+            }
         } else {
-            message.channel.send("That user isn't in this guild!");
+            message.channel.send("You didn't mention the user to ban!")
         }
-    } else {
-        message.channel.send("You didn't mention the user to ban!")
     }
 }
 
