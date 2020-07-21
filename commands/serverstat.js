4cf002886e9570e29efae2f3d83e9db409bc6900
sourTaste000 <sourTaste000@gmail.com>
2020-07-20 21:36:46 -0700

    now in new module

--- commands/serverstat.js
+++ commands/serverstat.js
@@ -1,26 +1,18 @@
-const ms = require("minestats/JavaScript");
+const ms = require("minestats");
 const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     try {
-        ms.init(args[0], 25565, function (result) {
-            if (ms.online) {
-                let statusEmbed = new Discord.MessageEmbed()
-                    .setTitle(`Minecraft server status of ${ms.address} on port ${ms.port}: `)
-                    .setDescription(
-                        `
-                    Server is running version ${ms.version} with ${ms.current_players} out of ${ms.max_players} players.
-                    Message of the day: ${ms.motd}
-                    Latency: ${ms.latency} ms.
-                        `
-                    )
-                message.channel.send(statusEmbed);
-            } else {
-                message.channel.send(`Server is offline!`);
-            }
-        });
+        ServerStats(args[0]).then(info => {
+            let statusEmbed = new Discord.MessageEmbed()
+                .setTitle(`Minecraft server status of ${args[0]}: `)
+                .setDescription(info)
+                .setColor(client.colors.kamiblue)
+            message.channel.send(statusEmbed);
+        })
     }catch(err){
+        message.channel.send("Failed to get the info of server! Is server online?");
         console.error(err);
     }
 }
