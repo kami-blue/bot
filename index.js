a3b3ca94843c1d7233c6d346153259c9a2ad786e
sourTaste000 <sourTaste000@gmail.com>
2020-07-26 18:20:24 -0700

    added uwu counter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import AuthFile
 const auth = require("./auth.json");
+const Enmap = require("enmap");
 
 const config = {
     prefix: ";",
@@ -103,8 +104,11 @@ client.on('message', async message => {
     autoResponder(message);
 });
 
-//starboard
-//( ͡° ͜ʖ ͡°)
+/**
+ * @name starboard
+ * @author sourTaste000
+ * ( ͡° ͜ʖ ͡°)
+ */
 let pinnedMessages = [];
 let i = 0;
 
@@ -125,6 +129,36 @@ client.on('messageReactionAdd', async (reaction, user) => {
             }
     }
 });
+
+/**
+ * @name uwuCounter
+ * @author sourTaste000
+ */
+const uwuCounter = new Enmap({name: "uwuCounter"});
+let j = 0;
+
+client.on("ready", async () => {
+    await uwuCounter.defer;
+    console.log(`${uwuCounter.size} keys loaded`);
+})
+
+client.on('message', async message => {
+    if(message.content.toLowerCase().includes(";createmap")){
+        message.channel.send("ok")
+        uwuCounter.ensure(`${message.guild.id}-${message.author.id}`, {user: message.author.id, uwuTimes: 1});
+    }
+
+    if(message.content.toLowerCase().includes("uwu") || message.content.toLowerCase().includes("owo")){
+        if (message.author.bot) return;
+        uwuCounter.inc(`${message.guild.id}-${message.author.id}`, "uwuTimes")
+    }
+
+    if(message.content.toLowerCase().includes(";counter")){
+        message.channel.send(`You said uwu ${uwuCounter.get(`${message.guild.id}-${message.author.id}`, "uwuTimes")} times!`);
+    }
+});
+
+
 
 /* when message is edited */
 client.on('messageUpdate', async message => {
