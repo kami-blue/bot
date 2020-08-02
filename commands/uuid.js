27fc8f15214ec0f3cb8fd8149cc4b7ef3f861707
IcyChungus <47074495+sourTaste000@users.noreply.github.com>
2020-08-02 10:51:10 -0700

    Update uuid.js

--- commands/uuid.js
+++ commands/uuid.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
+    if(isNaN(args[0]) || args[0].toLowerCase() === "undefined") return 1;
     try {
         let result;
         let part1, part2, part3, part4, part5;
