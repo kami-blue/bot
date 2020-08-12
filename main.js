ca2097cf2ce39ef8e8ad54d031ea6cdc71f9a206
IcyChungus <sourtaste000@gmail.com>
2020-08-12 16:05:28 -0700

    fixed code(finally)?

--- main.js
+++ main.js
@@ -99,18 +99,26 @@ client.on("ready", () => {
      * @since  8/12/2020
      */
     setInterval(() => {
-        fetch("https://api.github.com/repos/kami-blue/client/releases", {headers: {Authorization: `token ${auth.githubtoken}`}})
+        fetch("https://api.github.com/repos/kami-blue/nightly-releases/releases", {headers: {Authorization: `token ${auth.githubtoken}`}})
             .then(response => response.json())
             .then(data => {
-                const final = JSON.parse(JSON.stringify(data));
-                let j = 0;
-                for(let i = 0; i <= 29; i++){ j += final[i].assets[0].download_count }
-                fetch("https://kamiblue.org/api/v1/totalNightlies.json")
+                const nightly = JSON.parse(JSON.stringify(data));
+                let nightlyCount = 0;
+                for(let i = 0; i <= 29; i++){ nightlyCount += nightly[i].assets[0].download_count }
+                fetch("https://api.github.com/repos/kami-blue/nightly-releases/releases", {headers: {Authorization: `token ${auth.githubtoken}`}})
                     .then(response => response.json())
                     .then(data => {
-                        const total = JSON.parse(JSON.stringify(data))
-                        client.channels.cache.get('743240299069046835').setName(`${j*(total.count/30)} Downloads`)
+                        const stable = JSON.parse(JSON.stringify(data));
+                        let stableCount = 0;
+                        for(let i = 0; i <= 29; i++){ stableCount += stable[i].assets[0].download_count }
+                        fetch("https://kamiblue.org/api/v1/totalNightlies.json")
+                            .then(response => response.json())
+                            .then(data => {
+                                const total = JSON.parse(JSON.stringify(data))
+                                client.channels.cache.get('743240299069046835').setName(`${nightlyCount*(total.count/30)+stableCount} Downloads`)
+                            })
                     })
+
             })
             .catch((error) => {
                 console.error("Failed to grab downloads!")
