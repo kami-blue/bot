daec507aee377e3f11b71ba5ba1e70cf61f9d311
okk <foxsinek@gmail.com>
2020-08-15 16:19:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -240,7 +240,7 @@ client.on("messageDelete", (msg) => {
         if (!msg.member.hasPermission("CHANGE_NICKNAME")) {
             const deleteEmbed = new Discord.MessageEmbed()
                 .setAuthor("カミブルー！", "https://cdn.discordapp.com/avatars/638403216278683661/1e8bed04cb18e1cb1239e208a01893a1.png", "https://kamiblue.org")
-                .setDescription(`Message ${msg.content} by ${msg.author.tag} was deleted.`)
+                .setDescription(`Message ${msg.content} by ${msg.author.mention} was deleted.`)
                 .setColor(client.colors.red)
                 .setTimestamp();
             msg.channel.send(deleteEmbed);
