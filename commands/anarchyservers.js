25e27bdadb8561f29934b8613b93af3a4e79f8b5
Finleyyy <35464151+Finleyyy@users.noreply.github.com>
2020-08-15 23:19:26 +0200

    Update anarchyservers.js

--- commands/anarchyservers.js
+++ commands/anarchyservers.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.config = {
     name: "anarchyservers",
-    aliases: [serverlists],
+    aliases: ["serverlists"],
     use: "anarchyservers",
     description: "Searches for anarchy servers",
     state: "gamma",
