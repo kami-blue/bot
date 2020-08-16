876bf2b6511998dfc4969e52cbe4173609d96edb
TashNB <65786907+TashNB@users.noreply.github.com>
2020-08-16 14:33:19 -0400

    finxed everything i think plz let me jump off bridge

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const fetch = require("node-fetch")
 
 // Regexes
 /*ipLoggers*/
-const grabbifyLoggers = new RegExp("(grabify.{0,3}link|leancoding.{0,3}co|leancoding.{0,3}co|leancoding.{0,3}co|freegiftcards.{0,3}co|joinmy.{0,3}site|curiouscat.{0,3}club|catsnthings.{0,3}fun|catsnthing.{0,3}com)");
+const grabbifyLoggers = new RegExp("grabify.{0,3}link|leancoding.{0,3}co|leancoding.{0,3}co|leancoding.{0,3}co|freegiftcards.{0,3}co|joinmy.{0,3}site|curiouscat.{0,3}club|catsnthings.{0,3}fun|catsnthing.{0,3}com");
 
 /* bad messages regexes */
 const discordInviteRegex = new RegExp("(d.{0,3}.{0,3}s.{0,3}c.{0,3}.{0,3}r.{0,3}d).{0,7}(gg|com.{0,3}invite)");
@@ -281,7 +281,7 @@ async function autoResponder(message) {
         /*ip logger regex */
         if (!cleanedMessage.length && !message.attachments) return message.delete();
         if (grabbifyLoggers.test(cleanedMessage)||
-        new RegExp("(linkify.{0,3}me|raboninco.{0,3}com|j.{0,3}mp|bit.{0,3}ly|goo.{0,3}gl|//tinyurl.{0,3}com|is.{0,3}gd|bit.{0,3}do|iplogger.{0,3}org/logger|ps3cfw.{0,3}com/cool.php|rb.{0,3}gy|blasze.{0,3}tk)").test(cleanedMessage) {
+        new RegExp("linkify.{0,3}me|raboninco.{0,3}com|j.{0,3}mp|bit.{0,3}ly|goo.{0,3}gl|//tinyurl.{0,3}com|is.{0,3}gd|bit.{0,3}do|iplogger.{0,3}org/logger|ps3cfw.{0,3}com/cool.php|rb.{0,3}gy|blasze.{0,3}tk").test(cleanedMessage)) {
             message.reply(warnRule(message, 3, "You're not allowed to post IP grabbers dumbass (or shortened urls)!"))
             return message.delete()
         }
