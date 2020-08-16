f79608b8609f2b1fa730fb9b5fdee9630f855954
TashDevCat <68488322+TashDevCat@users.noreply.github.com>
2020-08-16 14:29:32 -0400

    Update xiaro.js

--- commands/xiaro.js
+++ commands/xiaro.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     message.channel.send(`Trans rights~! :3, ${message.content.slice(7)}`)
-    return message.delete()
+    message.delete()
 }
 
 module.exports.config = {
