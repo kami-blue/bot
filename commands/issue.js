26c885b31fc5fd317060cc804ffabb6ae835a6e2
IcyChungus <sourtaste000@gmail.com>
2020-08-18 21:46:47 -0700

    prototype for issue lookup by words

--- commands/issue.js
+++ commands/issue.js
@@ -8,15 +8,18 @@ const auth = require("../auth.json");
  * Fixed by humboldt123
  * Updated by sourTaste000 on 8/2/2020
  * Updated by sourTaste000(again) on 8/10/2020
+ * Updated by sourTaste000(bruh...again) on 8/18/2020
  */
 
 module.exports.run = async (client, message, args) => {
     if (!args || !args[0]) return;
-        fetch(`https://api.github.com/repos/kami-blue/${args[0]}/issues/${args[1]}`, {headers: {Authorization: `token ${auth.githubtoken}`}})
+    if(args[0] === "number") {
+        const msg = await message.channel.send("Fetching issues...")
+        fetch(`https://api.github.com/repos/kami-blue/${args[1]}/issues/${args[2]}`, {headers: {Authorization: `token ${auth.githubtoken}`}})
             .then(response => response.json())
             .then(data => {
                 let result = JSON.parse(JSON.stringify(data));
-                if(result.html_url.includes("pull")){
+                if (result.html_url.includes("pull")) {
                     let result = JSON.parse(JSON.stringify(data));
                     fetch(result.pull_request.url)
                         .then(response => response.json())
@@ -34,6 +37,7 @@ module.exports.run = async (client, message, args) => {
                                 .addField("Changed Files", result.changed_files, true)
                                 .addField("Comments", result.comments, true)
                                 .setColor(client.colors.kamiblue)
+                            msg.delete()
                             message.channel.send(pullEmbed)
                         })
                         .catch((error) => {
@@ -48,11 +52,11 @@ module.exports.run = async (client, message, args) => {
                     let labels = result.labels || {"login": "None"};
                     let status = result.state;
                     let labels1 = [];
-                    if(labels === undefined || isNaN(labels)){
+                    if (labels === undefined || isNaN(labels)) {
                         for (i in labels) {
                             labels1.push(labels[i].name)
                         }
-                    }else{
+                    } else {
                         labels1 = "No Labels"
                     }
                     let issueEmbed = new Discord.MessageEmbed()
@@ -66,7 +70,7 @@ module.exports.run = async (client, message, args) => {
                         .addField("Status", status)
                         .addField("Milestone", milestone.title)
                         .setColor(client.colors.kamiblue)
-
+                    msg.delete()
                     message.channel.send(issueEmbed)
                 }
             })
@@ -74,6 +78,20 @@ module.exports.run = async (client, message, args) => {
                 message.channel.send("Bad issue number or repository!");
                 console.error('Error:', error);
             })
+    } else if(args[0] === "keyword"){
+        const msg = await message.channel.send("Fetching issues...")
+        fetch(`https://api.github.com/search/issues?q=${args[2]}+repo:kami-blue/${args[1]}`, {headers: {Authorization: `token ${auth.githubtoken}`}})
+            .then(response => response.json())
+            .then(data => {
+                //trash code lmao
+                const final = JSON.parse(JSON.stringify(data));
+                msg.edit(`DM'ed you the results!`)
+                for(let i = 0; i <= final.total_count - 1; i++){
+                    message.author.send(`Issue Number: ${final.items[i].number}, Title: ${final.items[i].title}`)
+                }
+            })
+            .catch((err) => {message.channel.send("Cannot find the issue!"); console.error(err)})
+    }
 };
 
 module.exports.config = {
