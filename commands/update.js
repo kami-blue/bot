fa1325fbbd1e1cdd17a181f9b56ae6dafe8e039e
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-11-16 14:13:37 -0500

    make not hardcoded

--- commands/update.js
+++ commands/update.js
@@ -5,7 +5,7 @@ const exec = require('child_process').exec;
 module.exports.run = async (client, message, args) => {
     if (!(message.author.id === "563138570953687061") && !(message.author.id === "297096161842429963") && !(message.author.id === "326039530971070474") && !(message.author.id === "688398900721352716")) return;
     message.reply("Updating...")
-    const myShellScript = exec('sh update.sh /home/mika/bot/');
+    const myShellScript = exec('sh update.sh ~/bot/');
     myShellScript.stdout.on('data', (data) => {
         console.log(data);
         // do whatever you want here with data
