032a96c9c24625216db3a260786ff4554612621b
ZimnyCat <thezimnycat@gmail.com>
2020-11-22 10:32:07 +0500

    add ;module command

---
+++ commands/util.moduleslist.js
@@ -0,0 +1,77 @@
+module.exports.modules = {
+    antispam: {
+        name: "AntiSpam",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Удаляет рекламу и спам из чата",
+        description-en: "\n**Category**: Chat\n**Description**: Removes spam and advertising from the chat."
+    }
+    autoexcuse: {
+        name: "AutoExcuse",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Оправдывает вас, когда вы умираете",
+        description-en: "\n**Category**: Chat\n**Description**: Makes an excuse for you when you die"
+    }
+    autoqmain: {
+        name: "AutoQMain",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Автоматически пишет '/queue main' на серверах",
+        description-en: "\n**Category**: Chat\n**Description**: Automatically does '/queue main' on servers"
+    }
+    autoreply: {
+        name: "AutoReply",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Автоматический ответ на личные сообщения",
+        description-en: "\n**Category**: Chat\n**Description**: Automatically reply to direct messages"
+    }
+    autotpa: {
+        name: "AutoTPA",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Автоматически отклоняет или принимет запросы на телепортацию",
+        description-en: "\n**Category**: Chat\n**Description**: Automatically accept or decline /TPAs"
+    }
+    baritoneremote: {
+        name: "BaritoneRemote",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Удаленный контроль Baritone для другого игрока через /msg",
+        description-en: "\n**Category**: Chat\n**Description**: Remotely control Baritone with /msg"
+    }
+    chatencryption: {
+        name: "ChatEncryption",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Шифрует и расшифровывает сообщения из чата",
+        description-en: "\n**Category**: Chat\n**Description**: Encrypts and decrypts chat messages"
+    }
+    chatfilter: {
+        name: "ChatFilter",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Убирает выбранные слова или фразы из чата",
+        description-en: "\n**Category**: Chat\n**Description**: Filters custom words or phrases from the chat"
+    }
+    chattimestamp: {
+        name: "ChatTimeStamp",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Показывает время отправки сообщения рядом с сообщением",
+        description-en: "\n**Category**: Chat\n**Description**: Shows the time a message was sent beside the message"
+    }
+    customchat: {
+        name: "CustomChat",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Добавьте собственное окончание к своему сообщению!",
+        description-en: "\n**Category**: Chat\n**Description**: Add a custom ending to your message!"
+    }
+    discordnotifs: {
+        name: "DiscordNotifs",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Отправляет ваш чат на установленный канал Discord",
+        description-en: "\n**Category**: Chat\n**Description**: Sends your chat to a set Discord channel"
+    }
+    fancychat: {
+        name: "FancyChat",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Делает сообщения, которые вы отправляете, красивыми",
+        description-en: "\n**Category**: Chat\n**Description**: Makes messages you send fancy"
+    }
+    formatchat: {
+        name: "FormatChat",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Поддержка цвета и переноса строки в пакетах чата (другими словами вы можете использовать ENTER в чате)",
+        description-en: "\n**Category**: Chat\n**Description**: Add colour and linebreak support to upstream chat packets"
+    }
+    friendhighlight: {
+        name: "FriendHighlight",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Подсвечивает ники ваших друзей в чате",
+        description-en: "\n**Category**: Chat\n**Description**: Highlights your friends names in chat"
+    }
+    kamimoji: {
+        name: "KamiMoji",
+        description-ru: "\n**Категория**: Chat\n**Описание**: Добавьте смайлики в чат с помощью KamiMoji, любезно предоставлено EmojiAPI.",
+        description-en: "\n**Category**: Chat\n**Description**: Add emojis to chat using KamiMoji, courtesy of the EmojiAPI."
+    }
+}
