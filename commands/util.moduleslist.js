7924a29ec8c5ba2d03c56b45e64540506b829a35
cattyn <60744119+cattyngmd@users.noreply.github.com>
2020-11-22 15:21:41 +0300

    Update util.moduleslist.js

--- commands/util.moduleslist.js
+++ commands/util.moduleslist.js
@@ -1,77 +1,202 @@
 module.exports.modules = {
     antispam: {
         name: "AntiSpam",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Удаляет рекламу и спам из чата",
-        description-en: "\n**Category**: Chat\n**Description**: Removes spam and advertising from the chat."
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Удаляет рекламу и спам из чата.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Removes spam and advertising from the chat."
+    },
     autoexcuse: {
         name: "AutoExcuse",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Оправдывает вас, когда вы умираете",
-        description-en: "\n**Category**: Chat\n**Description**: Makes an excuse for you when you die"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Оправдывает вас, когда вы умираете.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Makes an excuse for you when you die."
+    },
     autoqmain: {
         name: "AutoQMain",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Автоматически пишет '/queue main' на серверах",
-        description-en: "\n**Category**: Chat\n**Description**: Automatically does '/queue main' on servers"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Автоматически пишет '/queue main' на серверах.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Automatically does '/queue main' on servers."
+    },
     autoreply: {
         name: "AutoReply",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Автоматический ответ на личные сообщения",
-        description-en: "\n**Category**: Chat\n**Description**: Automatically reply to direct messages"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Автоматический ответ на личные сообщения.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Automatically reply to direct messages."
+    },
     autotpa: {
         name: "AutoTPA",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Автоматически отклоняет или принимет запросы на телепортацию",
-        description-en: "\n**Category**: Chat\n**Description**: Automatically accept or decline /TPAs"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Автоматически отклоняет или принимет запросы на телепортацию.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Automatically accept or decline /TPAs."
+    },
     baritoneremote: {
         name: "BaritoneRemote",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Удаленный контроль Baritone для другого игрока через /msg",
-        description-en: "\n**Category**: Chat\n**Description**: Remotely control Baritone with /msg"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Удаленный контроль Baritone для другого игрока через /msg.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Remotely control Baritone with /msg."
+    },
     chatencryption: {
         name: "ChatEncryption",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Шифрует и расшифровывает сообщения из чата",
-        description-en: "\n**Category**: Chat\n**Description**: Encrypts and decrypts chat messages"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Шифрует и расшифровывает сообщения из чата.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Encrypts and decrypts chat messages."
+    },
     chatfilter: {
         name: "ChatFilter",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Убирает выбранные слова или фразы из чата",
-        description-en: "\n**Category**: Chat\n**Description**: Filters custom words or phrases from the chat"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Убирает выбранные слова или фразы из чата.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Filters custom words or phrases from the chat."
+    },
     chattimestamp: {
         name: "ChatTimeStamp",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Показывает время отправки сообщения рядом с сообщением",
-        description-en: "\n**Category**: Chat\n**Description**: Shows the time a message was sent beside the message"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Показывает время отправки сообщения рядом с сообщением.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Shows the time a message was sent beside the message."
+    },
     customchat: {
         name: "CustomChat",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Добавьте собственное окончание к своему сообщению!",
-        description-en: "\n**Category**: Chat\n**Description**: Add a custom ending to your message!"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Добавьте собственное окончание к своему сообщению!",
+        descriptionen: "\n**Category**: Chat\n**Description**: Add a custom ending to your message!"
+    },
     discordnotifs: {
         name: "DiscordNotifs",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Отправляет ваш чат на установленный канал Discord",
-        description-en: "\n**Category**: Chat\n**Description**: Sends your chat to a set Discord channel"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Отправляет ваш чат на установленный канал Discord.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Sends your chat to a set Discord channel."
+    },
     fancychat: {
         name: "FancyChat",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Делает сообщения, которые вы отправляете, красивыми",
-        description-en: "\n**Category**: Chat\n**Description**: Makes messages you send fancy"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Делает сообщения, которые вы отправляете, красивыми.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Makes messages you send fancy."
+    },
     formatchat: {
         name: "FormatChat",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Поддержка цвета и переноса строки в пакетах чата (другими словами вы можете использовать ENTER в чате)",
-        description-en: "\n**Category**: Chat\n**Description**: Add colour and linebreak support to upstream chat packets"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Поддержка цвета и переноса строки в пакетах чата (другими словами вы можете использовать ENTER в чате).",
+        descriptionen: "\n**Category**: Chat\n**Description**: Add colour and linebreak support to upstream chat packets."
+    },
     friendhighlight: {
         name: "FriendHighlight",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Подсвечивает ники ваших друзей в чате",
-        description-en: "\n**Category**: Chat\n**Description**: Highlights your friends names in chat"
-    }
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Подсвечивает ники ваших друзей в чате.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Highlights your friends names in chat."
+    },
     kamimoji: {
         name: "KamiMoji",
-        description-ru: "\n**Категория**: Chat\n**Описание**: Добавьте смайлики в чат с помощью KamiMoji, любезно предоставлено EmojiAPI.",
-        description-en: "\n**Category**: Chat\n**Description**: Add emojis to chat using KamiMoji, courtesy of the EmojiAPI."
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Добавьте смайлики в чат с помощью KamiMoji, любезно предоставлено EmojiAPI.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Add emojis to chat using KamiMoji, courtesy of the EmojiAPI."
+    },
+    loginmessage: {
+        name: "LoginMessage",
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Отправляет заданное сообщение в публичный чат при входе на сервер.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Sends a given message to public chat on login."
+    },
+    portalchat: {
+        name: "PortalChat",
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Позволяет вам открывать графические интерфейсы, находясь в портале.",
+        descriptionen: "\n**Category**: Chat\n**Description**: Allows you to open GUIs in portals."
+    },
+    spammer: {
+        name: "Spammer",
+        descriptionru: "\n**Категория**: Chat\n**Описание**: Спамит текст из файла с заданной задержкой в ​​чат\nОткройте `.minecraft` и создайте там файл spammer.txt, затем редактируйте его, где новое каждое новое сообщение начинается с новой строки, учтите, что Windows по умолчанию скрывает тип файла, то есть вам нужно создать простой текстовый файл с названием spammer!",
+        descriptionen: "\n**Category**: Chat\n**Description**: Spam text from a file with a specified delay into the chat \ nOpen `.minecraft` and create a spammer.txt file there, then edit it, where each new message starts on a new line, note that Windows hides the file type by default, then there you have to create a simple text file called spammer!"
+    },
+    activemodules: {
+        name: "ActiveModules",
+        descriptionru: "\n**Категория**: Client\n**Описание**: Настраивает цвета и режимы ActiveModules.",
+        descriptionen: "\n**Category**: Client\n**Description**: Configures ActiveModules colours and modes."
+    },
+    baritone: {
+        name: "Baritone",
+        descriptionru: "\n**Категория**: Client\n**Описание**: Конфигурирует настройки Baritone.",
+        descriptionen: "\n**Category**: Client\n**Description**: Configures Baritone settings."
+    },
+    capes: {
+        name: "Capes",
+        descriptionru: "\n**Категория**: Client\n**Описание**: Управляет отображением плащей KAMI Blue.\nЕсли вы хотите плащ KAMI Blue, вы можете задонатить тут >\n__PayPal__: https://paypal.me/mik4a/5USD\n__BTC__: https://www.blockchain.com/btc/address/19pH4aNZZMPJkqQ2826BauRokyBs1NYon7\n__BCH__: https://www.blockchain.com/bch/address/19pH4aNZZMPJkqQ2826BauRokyBs1NYon7.",
+        descriptionen: "\n**Category**: Client\n**Description**: Controls the display of KAMI Blue capes.\nIf you want a KAMI Blue cape, you can donate here >\n__PayPal__: https://paypal.me/mik4a/5USD\n__BTC__: https://www.blockchain.com/btc/address/19pH4aNZZMPJkqQ2826BauRokyBs1NYon7\n__BCH__: https://www.blockchain.com/bch/address/19pH4aNZZMPJkqQ2826BauRokyBs1NYon7."
+    },
+    commandconfig: {
+        name: "CommandConfig",
+        descriptionru: "\n**Категория**: Client\n**Описание**: Настраивает клиентский чат.",
+        descriptionen: "\n**Category**: Client\n**Description**: Configures client chat related stuff."
+    },
+    infooverlay: {
+        name: "InfoOverlay",
+        descriptionru: "\n**Категория**: Client\n**Описание**: Настраивает наложение игровой информации.",
+        descriptionen: "\n**Category**: Client\n**Description**: Configures the game information overlay."
+    },
+    inventoryviewer: {
+        name: "InventoryViewer",
+        descriptionru: "\n**Категория**: Client\n**Описание**: Настраивает параметры Inventory Viewer.",
+        descriptionen: "\n**Category**: Client\n**Description**: Configures Inventory Viewer's options."
+    },
+    aimbot: {
+        name: "AimBot",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Автоматически нацеливается на объекты вместо вас.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Automatically aims at entities for you."
+    },
+    antichainpop: {
+        name: "AntiChainPop",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Включает Surround при потере тотема.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Enables Surround when popping a totem."
+    },
+    antifriendhit: {
+        name: "AntiFriendHit",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Не атакует ваших друзей.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Don't hit your friends."
+    },
+    aura: {
+        name: "Aura",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Бьет по сущностям вокруг вас.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Hits entities around you."
+    },
+    autoez: {
+        name: "AutoEZ",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Отправляет оскорбление в чат после убийства.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Sends an insult in chat after killing someone."
+    },
+    autofeetplace: {
+        name: "AutoFeetPlace",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Продолжительно кладёт обсидиан вокруг ваших ног.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Continually places obsidian around your feet."
+    },
+    autolog: {
+        name: "AutoLog",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Продолжительно кладёт обсидиан вокруг ваших ног.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Continually places obsidian around your feet."
+    },
+    automend: {
+        name: "AutoMend",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Автоматически чинит броню с помощью бутылок опыта!",
+        descriptionen: "\n**Category**: Combat\n**Description**: Automatically mends armour with XP bottles!"
+    },
+    autototem: {
+        name: "AutoTotem",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Автоматически берет тотем или другой предмет в левую руку.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Refills your offhand with totems or other items."
+    },
+    autotrap: {
+        name: "AutoTrap",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Захватывает ваших врагов обсидианом.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Traps your enemies in obsidian."
+    },
+    criticals: {
+        name: "Criticals",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Всегда делает критические атаки.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Always do critical attacks."
+    },
+    crystalaura: {
+        name: "CrystalAura",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Ставит Кристаллы Края, чтобы убивать врагов.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Places End Crystals to kill enemies."
+    },
+    offhandgap: {
+        name: "OffHandGap",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Удерживает яблоко бога при щелчке правой кнопкой мыши по мечу!",
+        descriptionen: "\n**Category**: Combat\n**Description**: Holds a God apple when right clicking your sword!"
+    },
+    surround: {
+        name: "Surround",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Окружает вас обсидианом, чтобы получать меньше урона.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Surrounds you with obsidian to take less damage."
+    },
+    totempopcounter: {
+        name: "TotemPopCounter",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Считает, сколько раз игроки теряют тотемы.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Counts how many times players pop."
+    },
+    visualrange: {
+        name: "VisualRange",
+        descriptionru: "\n**Категория**: Combat\n**Описание**: Показывает игроков, которые входят в зону видимости и выходят из неё в чате.",
+        descriptionen: "\n**Category**: Combat\n**Description**: Shows players who enter and leave range in chat."
     }
 }
