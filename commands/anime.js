0155ddd3ab5d85ddfd840bfeac20ce9febc5b215
SoNothing <git@sonothing.com>
2020-11-22 18:11:13 +0800

    no anime for you, 1/200 chance
    i have no hope that this works

--- commands/anime.js
+++ commands/anime.js
@@ -5,12 +5,16 @@ const fetch = require("node-fetch");
 module.exports.run = async (client, message, args) => {
     if (message.channel.nsfw === true) {
         const m = await message.channel.send("Fetching image...");
+        if ((Math.random() * 200) > 199) {
+            m.edit("no anime for you");
+        } else {
         fetch(`https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=${Math.floor(Math.random() * 3006694)}&limit=1&json=1&rating=safe`)
             .then(response => response.json())
             .then(data => {
                 const final = JSON.parse(JSON.stringify(data));
                 m.edit(`https://safebooru.org/images/${final[0].directory}/${final[0].image}`)
             })
+        }
     } else {
         message.channel.send("Use this command in an nsfw channel!")
     }
