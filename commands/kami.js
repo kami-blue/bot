78ea70fa08e81f2a72ab483f15ad7da751a3fbb5
living <sokolov.dominika@gmail.com>
2020-11-30 14:35:41 -0500

    Update kami.js

--- commands/kami.js
+++ commands/kami.js
@@ -4,7 +4,7 @@ const fs = require("graceful-fs");
 module.exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
                 .setTitle(`KAMI vs KAMI Blue`)
-                .setDescription(`KAMI and KAMI Blue are vastly different.\n\nFor users, the main differences are:\n- KAMI is **1.16.x Fabric** and KAMI Blue is **1.12.2 Forge**.\n- The GUI's are entirely different, both functionally and aesthetically.\n- KAMI Blue has more features, but is limited to older versions.\n\nYou can get support for using KAMI at https://discord.gg/9hvwgeg.`)
+                .setDescription(`KAMI (aka KAMI Red) and KAMI Blue are vastly different.\n\nFor users, the main differences are:\n- KAMI is **1.16.x Fabric** and KAMI Blue is **1.12.2 Forge**.\n- The GUI's are entirely different, both functionally and aesthetically.\n- KAMI Blue has more features, but is limited to older versions.\n\nYou can get support for using KAMI at https://discord.gg/9hvwgeg.`)
                 .setColor(client.colors.kamiblue)
                 .setTimestamp();
     message.channel.send(embed)
