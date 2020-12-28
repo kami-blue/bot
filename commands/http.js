81c3802bdc691e99eb4e52260fbab8545dfe719f
ZimnyCat <thezimnycat@gmail.com>
2020-12-28 09:54:50 +0500

    add ;http

---
+++ commands/http.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+const errors = require("./utils/httperrors");
+
+module.exports.run = async (client, message, args) => {
+    if(errors.codes.includes(args[0])) {
+        message.channel.send("https://http.cat/" + errors.codes[args[0]]);
+    } else message.channel.send("https://http.cat/" + args[0]);
+}
+
+module.exports.config = {
+    name: "http",
+    aliases: [],
+    use: "http [Error code]",
+    description: "Sends funny cat picture with http error",
+    state: "gamma",
+    page: 3
+};
