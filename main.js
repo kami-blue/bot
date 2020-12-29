88f8eb0c7652c471b906b65d7548d2ea9bbef606
liv <sokolov.dominika@gmail.com>
2020-12-29 09:55:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ async function autoResponder(message) {
         }
 
         /* slurs regex */
-        if (slursRegex.test(cleanedMessage.replace(/mini.{0,2}game/, "").replace("nigeria", "").replace("son i", ""))) {
+        if (slursRegex.test(cleanedMessage.replace(/mini.{0,2}game/, "").replace("nigeria", "").replace("son i", "").replace("nibar", ""))) {
             message.reply(warnRule(message, "1a, 1b, 1c, 1d", "slurs are not allowed in this Discord server"));
             return message.delete()
         }
