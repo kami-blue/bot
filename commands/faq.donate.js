35b89bcada8917fe9275c91cd980f50203ecc78a
Dominika <sokolov.dominika@gmail.com>
2021-01-06 13:23:09 -0500

    fix if statement

--- commands/faq.donate.js
+++ commands/faq.donate.js
@@ -8,14 +8,14 @@ module.exports.run = async (client, message, args) => {
                 .setTitle("дарить")
                 .setColor(client.colors.kamiblue)
                 .setThumbnail('https://pngimg.com/uploads/donate/donate_PNG54.png')
-                .setDescription('You can support developers of KAMI Blue for a small amount using PayPal.\nIf you donate from $5 you can get a special KAMI Blue cape that will be visible to everyone who uses the KAMI Blue client.\nPayPal donation link: https://kamiblue.org/donate')
+                .setDescription('Вы можете поддержать разработчиков клиента задонатив небольшую сумму используя платежную систему PayPal.\nПри донате от $5 вы можете получить специальный плащ KAMI Blue, который будет виден всем кто использует KAMI Blue.\nСсылка на PayPal: https://kamiblue.org/donate')
         message.channel.send(embed)
     } else {
         const embed = new Discord.MessageEmbed()
                 .setTitle("Donate")
                 .setColor(client.colors.kamiblue)
                 .setThumbnail('https://pngimg.com/uploads/donate/donate_PNG54.png')
-                .setDescription('Вы можете поддержать разработчиков клиента задонатив небольшую сумму используя платежную систему PayPal.\nПри донате от $5 вы можете получить специальный плащ KAMI Blue, который будет виден всем кто использует KAMI Blue.\nСсылка на PayPal: https://kamiblue.org/donate')
+                .setDescription('You can support developers of KAMI Blue for a small amount using PayPal.\nIf you donate from $5 you can get a special KAMI Blue cape that will be visible to everyone who uses the KAMI Blue client.\nPayPal donation link: https://kamiblue.org/donate')
         message.channel.send(embed)
     }
 };
