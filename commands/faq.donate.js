873d995a1c041250c10ad834878f87f620b148f6
d1snin <kit.ne.kit@mail.ru>
2021-01-06 21:13:03 +0300

    fixed

--- commands/faq.donate.js
+++ commands/faq.donate.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-    const DonateFaqEmbedRu = new Discord.MessageEmbed()
+    const DonateFaqEmbedEn = new Discord.MessageEmbed()
     .setColor(client.colors.kamiblue)
     .setThumbnail('https://pngimg.com/uploads/donate/donate_PNG54.png')
     .setDescription('You can support developers of KAMI Blue for a small amount using the PayPal payment system.\nIf you donate from $5 you can get a special KAMI Blue cape that will be visible to everyone who uses the KAMI Blue client.\nPayPal donation link: https://paypal.me/mik4a/5USD')
-    const DonateFaqEmbedEn = new Discord.MessageEmbed()
+    const DonateFaqEmbedRu = new Discord.MessageEmbed()
     .setColor(client.colors.kamiblue)
     .setThumbnail('https://pngimg.com/uploads/donate/donate_PNG54.png')
     .setDescription('Вы можете поддержать разработчиков клиента задонатив небольшую сумму используя платежную систему PayPal.\nПри донате от $5 вы можете получить специальный плащ KAMI Blue, который будет виден всем кто использует KAMI Blue.\nСсылка на PayPal: https://paypal.me/mik4a/5USD')
