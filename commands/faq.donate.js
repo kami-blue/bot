a1ed5ed39f44e6daeb46cb158110bfa696e0fa1d
Dominika <sokolov.dominika@gmail.com>
2021-01-06 13:24:17 -0500

    fix eng translation of donate

--- commands/faq.donate.js
+++ commands/faq.donate.js
@@ -15,7 +15,7 @@ module.exports.run = async (client, message, args) => {
                 .setTitle("Donate")
                 .setColor(client.colors.kamiblue)
                 .setThumbnail('https://pngimg.com/uploads/donate/donate_PNG54.png')
-                .setDescription('You can support developers of KAMI Blue for a small amount using PayPal.\nIf you donate from $5 you can get a special KAMI Blue cape that will be visible to everyone who uses the KAMI Blue client.\nPayPal donation link: https://kamiblue.org/donate')
+                .setDescription('You can support developers of KAMI Blue for a small amount using PayPal.\nIf you donate from $5 you receive a donator cape, and are allowed to disable or change the watermark in game, and also receive a neat role on the Discord. In the future, you will also be able to download donor only and free plugins.\n\nPayPal donation link: https://kamiblue.org/donate')
         message.channel.send(embed)
     }
 };
