81a1262ba8d531ac0f263da6858fae979ee8e5e5
liv <sokolov.dominika@gmail.com>
2021-01-11 21:15:22 -0500

    Update faq.log.js

--- commands/faq.log.js
+++ commands/faq.log.js
@@ -3,9 +3,15 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     if (message.channel.id == "722436626248237076") {
-        message.channel.send("Найдите файл `debug.log` в `~/.minecraft/logs` и вставьте содержимое на <https://paste.ee>, а затем отправьте ссылку.")
+        const embed = new Discord.MessageEmbed()
+            .setColor(client.colors.kamiblue)
+            .setDescription("Найдите файл `debug.log` в `~/.minecraft/logs` и вставьте содержимое на <https://paste.ee>, а затем отправьте ссылку.")
+        message.channel.send(embed)
     } else {
-        message.channel.send("Find the `debug.log` file inside `~/.minecraft/logs` and paste the contents to <https://paste.ee>, and the send the link.")
+        const embed = new Discord.MessageEmbed()
+            .setColor(client.colors.kamiblue)
+            .setDescription("Find the `debug.log` file inside `~/.minecraft/logs` and paste the contents to https://paste.ee/, and the send the link.")
+        message.channel.send(embed)
     }
 };
 
