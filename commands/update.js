f196d5994225637ca85b57b168101a58f9741057
liv <~@l1v.in>
2021-01-27 00:32:52 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 const exec = require('child_process').exec;
 
 module.exports.run = async (client, message, args) => {
-    if (!(message.author.id === "563138570953687061") && !(message.author.id === "297096161842429963") && !(message.author.id === "326039530971070474") && !(message.author.id === "688398900721352716")) return;
+    if (!(message.author.id === "242462997530804225") && !(message.author.id === "297096161842429963") && !(message.author.id === "326039530971070474") && !(message.author.id === "688398900721352716")) return;
     message.reply("Updating...")
     const myShellScript = exec('sh update.sh ~/bot/');
     myShellScript.stdout.on('data', (data) => {
